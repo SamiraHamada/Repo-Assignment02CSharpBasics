2813a842070bee11fec8bf99cc96db54b7c3cd4f
samira hamada <samirahamadaabdelaleem123@gmail.com>
2026-09-09 08:10:59 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment02_C_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
