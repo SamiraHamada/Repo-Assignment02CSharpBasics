0115f4b00de5f32614bc49fc36f3344af3047f63
samira hamada <samirahamadaabdelaleem123@gmail.com>
2026-09-09 09:01:08 +0300

    #region 1st Question     //Declare string title = "clean code";. Call title.ToUpper() and store it in a new variable upperTitle. Print    // both title and upperTitle to show that title did not change.     string title = "clean code";
        string upperTitle = title.ToUpper();
    
        Console.WriteLine(title); //title did not change because string is immutable and original string is not changed
        Console.WriteLine(upperTitle);
    
        #endregion

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Assignment02_C_Basics
+﻿using System.Security.Cryptography;
+using System.Threading.Channels;
+
+namespace Assignment02_C_Basics
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region 1st Question
+            //Declare string title = "clean code";. Call title.ToUpper() and store it in a new variable upperTitle. Print
+           // both title and upperTitle to show that title did not change.
+            string title = "clean code";
+
+            string upperTitle = title.ToUpper();
+
+            Console.WriteLine(title); //title did not change because string is immutable and original string is not changed
+            Console.WriteLine(upperTitle);
+
+            #endregion
+
         }
     }
 }
